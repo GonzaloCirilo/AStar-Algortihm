1322a4cec129fccbd1a306bfd1aae6cef9c62a53
DESKTOP-HB0C1U8\Usuario <gonzalo_cirilo@hotmail.com>
2017-09-04 15:02:26 -0500

    almost... but not there

--- IA/Source/IA/PathAIController.cpp
+++ IA/Source/IA/PathAIController.cpp
@@ -1,100 +1,143 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PathAIController.h"
-#include <queue>
-#include <vector>
-#include <functional>
+#include "GenericPlatformMath.h"
 #include "Engine/World.h"
 #include "Navigation/PathFollowingComponent.h"
 #include "DrawDebugHelpers.h"
 #include "GameFramework/Controller.h"
-typedef std::pair<float, FVector2D> fV2D;
+
+struct Map {
+	float weight;
+	FVector2D position;
+};
+
+struct MapOperator
+{
+	bool operator()(const Map& A, const Map& B) const
+	{
+		// Inverted compared to std::priority_queue - higher priorities float to the top
+		return A.weight < B.weight;
+	}
+};
+
 //x
 #define MAP_WIDTH 76
 //y
 #define MAP_HEIGHT 76
-
 #define MAP_BOX_SIZE 50
-TArray<FVector2D> MoveDirections = { FVector2D(1,0),FVector2D(-1,0),FVector2D(0,1),FVector2D(0,-1) };
-
 
 void APathAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	Dest = FVector::ZeroVector;
+	MoveDirections = { FVector2D(1,0),FVector2D(-1,0),FVector2D(0,1),FVector2D(0,-1) };
 	ControlledPawn = GetPawn();
+	Dest = ControlledPawn->GetActorLocation();
+	Dest = MapIndexToWorldLocation(WorldCordinatesToMapIndex(FVector2D(Dest.X, Dest.Y)));
+	AStar();
 }
 
-
+TArray<TArray<float>>map;
+TArray<TArray<FVector2D>>Parentmap;
 
 void APathAIController::AStar()
 {
-	std::priority_queue<fV2D, std::vector<fV2D>, std::greater<fV2D>>open;
-	std::vector<FVector2D>closed;
+	TArray<Map>open;
+	TArray<FVector2D>closed;
 
-	//TODO find a way to create a map matrix around level
+	TArray<TArray<float>>mapG;
 
-	std::vector<std::vector<float>>map;
-	std::vector<std::vector<float>>mapG;
-	std::vector<std::vector<FVector2D>>Parentmap;
+	TArray<FVector2D> templates;
+	templates.Init({ 0,0 }, MAP_WIDTH);
+	Parentmap.Init(templates, MAP_HEIGHT);
+	TArray<float> t;
+	t.Init(0, MAP_WIDTH);
+	mapG.Init(t, MAP_HEIGHT);
+	t.Init(-1, MAP_WIDTH);
+	map.Init(t, MAP_HEIGHT);
 
-	auto dest = ControlledPawn->GetActorLocation();
-
-
-	Parentmap = std::vector<std::vector<FVector2D>>(MAP_HEIGHT, std::vector<FVector2D>(MAP_WIDTH, { 0,0 }));
-	mapG = std::vector<std::vector<float>>(MAP_HEIGHT, std::vector<float>(MAP_WIDTH, 0));
-	map = std::vector<std::vector<float>>(MAP_HEIGHT, std::vector<float>(MAP_WIDTH, -1));
-
-	open.push({ DistanceTo(Dest), FVector2D::ZeroVector });
-	while (!open.empty()) 
+	open.HeapPush({ DistanceTo(FVector::ZeroVector), FVector2D::ZeroVector },MapOperator());
+	auto cont = 0;
+	while (open.Num()!=0 && cont!=200) 
 	{
-		auto aux = open.top(); open.pop();
-		auto auxIndex = WorldCordinatesToMapIndex(aux.second);
-
-		if (aux.second.X == dest.X&&aux.second.Y == dest.Y)break;
-
-		for (int i = 0; i < MoveDirections.Num(); i++)
+		Map aux; open.HeapPop(aux, MapOperator(), false);
+		auto auxIndex = WorldCordinatesToMapIndex(aux.position);
+		auto destIndex = WorldCordinatesToMapIndex(FVector2D(Dest.X, Dest.Y));
+		if (auxIndex==destIndex)break;
+		for (int32 i = 0; i < MoveDirections.Num(); i++)
 		{
-			float newX = aux.second.X + 50.f, newY = aux.second.Y + 50.f;
-			if (CheckMap(aux.second)/*Check Edges*/) 
+			float newX = aux.position.X + (MoveDirections[i].X*50.f), newY = aux.position.Y + (MoveDirections[i].Y*50.f);
+			if (CheckMap(FVector2D(newX,newY))/*Check Edges*/) 
 			{
-				int G = mapG[auxIndex.Y][auxIndex.X] + 50;
+				int32 G = mapG[auxIndex.Y][auxIndex.X] + 50;
 				auto newZ = ControlledPawn->GetActorLocation().Z;
-				int F = G + DistanceTo(FVector(newX, newY,newZ));
+			//		UE_LOG(LogTemp,Warning,TEXT("IN LOOP"))
+				float F = G + DistanceTo(FVector(newX, newY,newZ));
+				//UE_LOG(LogTemp,Warning,TEXT("%f OUT LOOP"),F)
 				auto newIndex = WorldCordinatesToMapIndex(FVector2D(newX, newY));
-				if (map[newIndex.Y][newIndex.X] == -1 || map[newIndex.Y][newIndex.X] > F && find(closed.begin(), closed.end(), newIndex) == closed.end())
+				if ((map[newIndex.Y][newIndex.X] == -1 || map[newIndex.Y][newIndex.X] > F) && closed.Find(newIndex))
 				{
-					Parentmap[newIndex.Y][newIndex.X] = aux.second;
+					Parentmap[newIndex.Y][newIndex.X] = aux.position;
 					map[newIndex.Y][newIndex.X] = F;
 					mapG[newIndex.Y][newIndex.X] = G;
-					open.push(fV2D({ F,newIndex }));
+					auto var = MapIndexToWorldLocation(newIndex);
+					open.HeapPush(Map({ F, /*FVector2D(var.X,var.Y)*/FVector2D(newX,newY) }),MapOperator());
 				}
 			}
 		}
-		closed.push_back(auxIndex);
-	}
-	//TODO Trace path and do get the first destination
+		/*Print open array*/
+		UE_LOG(LogTemp, Warning, TEXT("Iteration %d:"),cont)
+		UE_LOG(LogTemp,Warning,TEXT("OPEN"))
+		for (int32 k = 0; k < open.Num(); k++) 
+		{
+			UE_LOG(LogTemp,Warning,TEXT("%d: %f %s"),k,open[k].weight,*open[k].position.ToString())
+		}
+		closed.Push(auxIndex); cont++;
+		UE_LOG(LogTemp,Warning,TEXT("Closed"))
+		for (int i = 0; i < closed.Num(); i++)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *closed[i].ToString())
+		}
+	}/*
+	for (int i = 0; i < closed.Num(); i++)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *closed[i].ToString())
+	}*/
+	auto StartIndex = WorldCordinatesToMapIndex(FVector2D::ZeroVector);
+	auto EndIndex = WorldCordinatesToMapIndex(FVector2D(Dest.X, Dest.Y));
+
+	UE_LOG(LogTemp,Warning,TEXT("STARTING POINT:%s ENDINGPOINT: %s"),*StartIndex.ToString(),*EndIndex.ToString())
+	Parentmap[StartIndex.Y][StartIndex.X] = { 0,0 };
+	map[EndIndex.Y][EndIndex.X] = 0;
+	auto NextDest = pathFinder(Parentmap[EndIndex.Y][EndIndex.X]);
+	//UE_LOG(LogTemp,Warning,TEXT("%s"),*NextDest.ToString())
 }
 
 FVector2D APathAIController::WorldCordinatesToMapIndex(FVector2D WorldLocation)
 {
-	if (CheckMap(FVector2D(WorldLocation.X,WorldLocation.Y))) 
+	if (CheckMap(WorldLocation)) 
 	{
-		int x = std::ceil(WorldLocation.X + 1900.f / MAP_BOX_SIZE);
-		int y = std::ceil(WorldLocation.Y + 1900.f / MAP_BOX_SIZE);
+		//UE_LOG(LogTemp, Warning, TEXT("Input: %s"), *WorldLocation.ToString())
+		int x = FPlatformMath::CeilToInt((WorldLocation.X + 1900.f) / MAP_BOX_SIZE);
+		int y = FPlatformMath::CeilToInt((WorldLocation.Y + 1900.f) / MAP_BOX_SIZE);
+		//UE_LOG(LogTemp, Warning, TEXT("Output: %s"), *FVector2D(x, y).ToString())
 		return FVector2D(x, y);
 	}
 	return FVector2D();
 }
 
-
-
 FVector APathAIController::MapIndexToWorldLocation(FVector2D MapIndex)
 {
+	/*
+	LogTemp:Warning: Input: X=40.000 Y=38.000
+	LogTemp:Warning: Input: X=125.000 Y=25.000 Z=190.000
+	*/
 	if (CheckMap(MapIndex))
 	{
+		//UE_LOG(LogTemp, Warning, TEXT("Input: %s"), *MapIndex.ToString())
 		float WX = MapIndex.X*MAP_BOX_SIZE + (MAP_BOX_SIZE / 2) - 1900.f;
 		float WY = MapIndex.Y*MAP_BOX_SIZE + (MAP_BOX_SIZE / 2) - 1900.f;
+		//UE_LOG(LogTemp, Warning, TEXT("Input: %s"), *FVector(WX, WY, ControlledPawn->GetActorLocation().Z).ToString())
 		return FVector(WX, WY, ControlledPawn->GetActorLocation().Z);
 	}
 	return FVector();
@@ -104,10 +147,32 @@ bool APathAIController::CheckMap(FVector2D V)
 {
 	return V.X >= -1900.f&&V.X <= 1900.f&&V.Y >= -1900.f&&V.Y <= 1900.f;
 }
-
-float APathAIController::DistanceTo(FVector Dest)
+//TODO Change to Manhattan cause this garbage isn´t working
+float APathAIController::DistanceTo(FVector _Dest)
 {
-	return FVector::Distance(Dest,ControlledPawn->GetActorLocation());
+	FVector2D _DestIndex = WorldCordinatesToMapIndex(FVector2D(_Dest.X, _Dest.Y));
+	FVector2D DestIndex = WorldCordinatesToMapIndex(FVector2D(Dest.X, Dest.Y));
+	FVector2D R = DestIndex - _DestIndex;
+	float r = R.Size();
+	UE_LOG(LogTemp, Warning, TEXT("DistanceTo: %f Points: %s %s"), r, *_DestIndex.ToString(), *DestIndex.ToString())
+	return r;
+}
+
+FVector2D APathAIController::pathFinder(FVector2D position)
+{
+	TArray<FVector2D>s; s.Push(position);
+	while (true)
+	{
+		auto p = s.Top(); s.Pop();
+		if (p == FVector2D::ZeroVector) 
+		{
+			return p;
+		}
+		map[p.Y][p.X] = 0;
+		s.Push(Parentmap[p.Y][p.X]);
+	}
+
+	return FVector2D();
 }
 //76*76 array map from -1900 to 1900
 void APathAIController::Tick(float DeltaTime)
@@ -124,6 +189,4 @@ void APathAIController::Tick(float DeltaTime)
 			DrawDebugLine(GetWorld(), PawnLocation, PawnLocation + dir, FColor::Blue);
 		}
 	}
-	
-
 }
